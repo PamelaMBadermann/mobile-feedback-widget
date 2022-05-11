8fd06ed25b98429818ba0117ec43057fb71c8ff7
Pamela Badermann <badermann.pamela@gmail.com>
2022-05-10 22:25:55 -0300

    added reatc-native packages

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native';
 
 import { styles } from './styles';
+import { theme } from '../../theme';
 
 export function Widget() {
   return (
     <>
-
+      <TouchableOpacity
+        style={styles.button}
+      >
+        <ChatTeardropDots
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
     </>
   );
 }
--- src/components/Widget/styles.ts
+++ src/components/Widget/styles.ts
@@ -1,5 +1,18 @@
 import { StyleSheet } from 'react-native';
+import { getBottomSpace } from 'react-iphone-helper';
+
+import { theme } from '../../theme';
 
 export const styles = StyleSheet.create({
-  button: {}
+  button: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: theme.colors.brand,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 16,
+    bottom: getBottomSpace() + 16,
+  }
 });

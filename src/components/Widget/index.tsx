105e16aa2939f8214e4261ff7220670678fe9f4e
Pamela Badermann <badermann.pamela@gmail.com>
2022-05-10 23:52:08 -0300

    created Option and Options components

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -4,6 +4,8 @@ import { ChatTeardropDots } from 'phosphor-react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
+import { Options } from '../Options';
+
 import { styles } from './styles';
 import { theme } from '../../theme';
 
@@ -33,7 +35,7 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-
+        <Options />
       </BottomSheet>
     </>
   );

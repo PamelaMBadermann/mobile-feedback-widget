1028f380c09cce8ea7d84f34a401d1427e1fe85a
Pamela Badermann <badermann.pamela@gmail.com>
2022-05-10 21:22:52 -0300

    added personal fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
 
 import { theme } from './src/theme';
 import { Widget } from './src/components/Widget';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{
       flex: 1,

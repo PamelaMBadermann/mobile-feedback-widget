634d9d1a532e7605bcaeeb45f02d11aa6bd8b0a3
Pamela Badermann <badermann.pamela@gmail.com>
2022-05-10 21:16:13 -0300

    created Widget component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+
+import { theme } from './src/theme';
+import { Widget } from './src/components/Widget';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+      <Widget />
+      <StatusBar
+        style="light"
+        backgroundColor="transparent"
+        translucent
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

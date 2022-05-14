ea8aaf49704d47d5aab27fb57f1a3bcfb8b78736
Pamela Badermann <badermann.pamela@gmail.com>
2022-05-14 16:34:30 -0300

    created Success component

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -6,6 +6,7 @@ import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { Options } from '../Options';
 import { Form } from '../Form';
+import { Success } from '../Success';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
@@ -39,8 +40,7 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Form
-          feedbackType="BUG"
+        <Success
         />
       </BottomSheet>
     </>
